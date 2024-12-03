acc55ea57c7572bb1397ba94c131a168708ed8e4
PurpleTurtle669 <lovisahansen04@gmail.com>
2024-12-03 18:11:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //get HTML elements with different types of functions i.e. getElementById, -Class, loop-selection, querySelector(?)
+
+const checkbox = document.getElementsByName('divStyle'); //Nodelist
+console.log(checkbox);
+
+const textFields = document.querySelectorAll('.textfield'); //Nodelist
+console.log(textFields)
+
+const button = document.getElementsByTagName('button'); //HTML Collection
+console.log(button);
+
+const emptyDiv = document.getElementById('empty-div');
+console.log(emptyDiv);
